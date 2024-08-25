3ff9b4f1836e54ed8e2fdac91887487f35e8a7a9
Fredo <luisalfredo9905@gmail.com>
2024-08-24 23:10:58 -0500

    add: View for the projects on the portfolio

---
+++ src/components/Project.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let data;
+</script>
+
+<a
+  href={data.href}
+  target="_blank"
+  class="p-4 sm:p-6 md:pd-8 flex flex-col gap-4 rounded-lg border border-solid border-blue-700 text-center group cursor-pointer hover:border-blue-400 duration-200"
+>
+  <div
+    class="relative bg-zinc-900 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200 overflow-hidden rounded-full"
+  >
+    <i class={data.icon + " relative z-1"} />
+  </div>
+  <h3>{data.name}</h3>
+
+  <p>{data.desc}</p>
+
+  <div class="flex-1 flex justify-between gap-4 items-center">
+    <div
+      class="ml-auto cursor-pointer hover:text-zinc-950 duration-200 relative p-2 rounded after:absolute after:top-0 after:h-0 after:right-full after:w-full after:h-full after:bg-zinc-50 after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
+    >
+      <p class="relative z-4">
+        Go to
+        <i class="fa-solid fa-arrow-right place-items-center aspect-square" />
+      </p>
+    </div>
+  </div>
+</a>
---
+++ src/components/ProjectView.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Project from "../components/Project.svelte";
+  let projects = [
+    {
+      href: "#",
+      name: "Game of Life",
+      icon: "fa-solid fa-terminal",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus mollitia error facere culpa, blanditiis praesentium voluptatibus neque maiores magni odio perspiciatis excepturi illum nostrum id impedit. Repellendus reprehenderit distinctio aut.",
+    },
+    {
+      href: "#",
+      name: "TODO's manager",
+      icon: "fa-solid fa-list-check",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus mollitia error facere culpa, blanditiis praesentium voluptatibus neque maiores magni odio perspiciatis excepturi illum nostrum id impedit. Repellendus reprehenderit distinctio aut.",
+    },
+    {
+      href: "#",
+      name: "Zoo webpage",
+      icon: "fa-regular fa-file-lines",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus mollitia error facere culpa, blanditiis praesentium voluptatibus neque maiores magni odio perspiciatis excepturi illum nostrum id impedit. Repellendus reprehenderit distinctio aut.",
+    },
+  ];
+</script>
+
+<div class="flex flex-col gap-2 text-center">
+  <h6 class="text-lg sm:text-xl md:text-2xl">
+    Curious to <span class="poppins text-blue-400">see</span> my work?
+  </h6>
+  <h3>A few of my little creations . . .</h3>
+</div>
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+  {#each projects as project}
+    <Project data={project} />
+  {/each}
+</div>
