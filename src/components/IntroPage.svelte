9954efef244a47f0e1b8739c7255770c10227c31
Fredo <luisalfredo9905@gmail.com>
2024-08-24 23:10:34 -0500

    add: Introduction page and profile image

---
+++ src/components/IntroPage.svelte
@@ -0,0 +1,33 @@
+<div
+  class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+>
+  <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+    Hi, I'm <span class="poppins text-blue-400">Fredo</span> Rodriguez<br />
+    a <span class="poppins text-blue-400">Software</span> Engineer.
+  </h2>
+  <p class="text-base sm:text-lg md:text-xl">
+    My <span class="poppins text-blue-400">favorite tech</span> includes Python (pytorch,
+    django), Javascript/Typescript (React or Svelte), Node.js + Express.js & PostgreSQL
+  </p>
+  <a
+    href="#"
+    target="_blank"
+    class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-zinc-50 text-zinc-950 cursor-pointer"
+  >
+    <div
+      class="absolute top-0 right-full w-full h-full bg-blue-300 opacity-20 group-hover:translate-x-full z-0 duration-200"
+    />
+    <h4 class="relative z-9">
+      Resume <i
+        class="fa-solid fa-arrow-right place-items-center aspect-square"
+      />
+    </h4>
+  </a>
+</div>
+<div class="relative shadow-2xl grid place-items-center">
+  <img
+    src={"assets/profile.png"}
+    alt="Profile image"
+    class="object-cover z-[2] max-h-[70vh]"
+  />
+</div>
