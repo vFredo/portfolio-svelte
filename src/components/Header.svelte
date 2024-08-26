48d04e356260fdf4d8ae8a2b20ef31e962d63228
Fredo <luisalfredo9905@gmail.com>
2024-08-25 21:34:32 -0500

    add: new jiggle animation, new tab and fixing colors and size of header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,34 +3,36 @@
   let tabs = [
     { name: "Projects", link: "#projects" },
     { name: "About me", link: "#about" },
-    { name: "Experience", link: "#experience" }
+    { name: "Experience", link: "#experience" },
   ];
 </script>
 
 <header
-  class={"sticky z-[10] top-0 duration-100 px-6 flex items-center justify-between rounded-xl border border-solid " +
+  class={"sticky z-[10] mx-4 top-2 px-6 duration-200 flex items-center justify-between rounded-full border border-solid " +
     (y > 0
-      ? "py-4 bg-zinc-950 border-blue-950 bg-opacity-50"
-      : "py-6 bg-transparent border-transparent")}
+      ? "py-2 bg-zinc-950 border-blue-950 bg-opacity-50"
+      : "py-2 bg-transparent border-transparent")}
 >
-  <a href="#home" class="font-medium poppins">
+  <a href="#home" class="font-medium poppins hover:animate-jiggle">
     <b class="font-bold poppins">Alfredo</b> Rodriguez
   </a>
 
   <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
     {#each tabs as tab}
-      <a href={tab.link} class="relative rounded p-1 duration-200 hover:underline hover:font-bold">
+      <a
+        href={tab.link}
+        class="relative rounded p-1 duration-200 hover:underline hover:font-bold"
+      >
         <p>{tab.name}</p>
       </a>
     {/each}
+
+    <!-- Contact me button -->
     <a
       href="#contact"
-      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-zinc-950"
+      class="relative isolate flex justify-center items-center select-none overflow-hidden border align-middle transition-all duration-500 before:absolute before:-start-3 before:top-0 before:-z-10 before:h-full before:w-[calc(100%+1.5rem)] before:origin-left before:-skew-x-[16deg] before:scale-x-0 before:transition-transform before:duration-500 hover:before:scale-x-100 focus-visible:before:scale-x-100 active:scale-[1.01] disabled:opacity-70 border-zinc-50 px-3 py-1.5 text-sm md:px-4 md:py-2 md:text-base hover:text-zinc-900 focus-visible:text-zinc-900 text-zinc-50 before:bg-zinc-50 rounded-full"
     >
-      <div
-        class="absolute top-0 right-full w-full h-full bg-blue-300 opacity-20 group-hover:translate-x-full z-0 duration-200 hover:font-bold"
-      />
-      <h4 class="relative z-9">Contact me</h4>
+      Contact me
     </a>
   </div>
 </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        jiggle: {
+          from: {
+            transform: 'skewX(0deg) skewY(0deg)',
+          },
+          '50%': {
+            transform: 'skewX(-5deg) skewY(-5deg)',
+          },
+          to: {
+            transform: 'skewX(0deg) skewY(0deg)',
+          },
+        },
+      },
+      animation: {
+        jiggle: 'jiggle 0.6s cubic-bezier(0.85, 0, 0.15, 1)',
+      }
+    },
   },
   plugins: [],
 }
